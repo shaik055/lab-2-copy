2c85d05645ca77966320735e23595925a6bef95d
shaik055 <sananazneensk@gmail.com>
2023-11-22 16:55:08 +0300

    add cors

--- weatherapp.js
+++ weatherapp.js
@@ -1,9 +1,11 @@
 const unirest = require("unirest");
 const express = require("express");
 const bodyText = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyText.urlencoded({ extended: true })); //To use body parser with post request
+app.use(cors());
 
 //include all static files so we can use CSS
 app.use(express.static(__dirname + "/public"));
