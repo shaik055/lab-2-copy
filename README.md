381cbd9a8a6ad091ae4d116bc295cb034590d8fd
shaik055 <sananazneensk@gmail.com>
2023-11-22 14:42:33 +0300

    copy of ITEC 660 lab 2

---
+++ README.md
@@ -0,0 +1,36 @@
+# Lab-2
+
+In this lab you will build a weather app using Node.js and API calls.
+
+1. Install node.js ([https://nodejs.org/en/download/](https://nodejs.org/en/download/): select LTS), if you don't have it already.
+2. Create the project folder.
+
+```bash
+mkdir weatherApp
+cd weatherApp
+```
+
+3. Create necessary files.
+
+```bash
+atom index.html weatherapp.js style.css
+```
+
+4. Initialize the package manager.
+
+```bash
+npm init
+```
+
+5. Install express package using npm.
+6. Get your API keys at [https://rapidapi.com/signup](https://rapidapi.com/signup) for Open Weather Map API.
+7. Edit the weatherapp.js file as needed. 
+    1. Test your code for full functionality.
+    2. For example: What happens if they enter nothing for the city name? How can you fix inside the code is it is empty?
+8. Display on your page at least **three** weather-related information in a nice way. Get creative with your design. You can add icons or use API icons if there are any.
+9. Last but not the least, **deploy and run** you app on **Heroku**.
+    1. Submit your heroku link in the github repository read.me file.
+10. Update the **readme file** by answering the following questions:
+    1. Assume that in your API call you received a 401 status code. Which part of your code most probably is not correct?
+    2. Explain your design choices and how you used CSS to realize those changes.
+11. Submit your repository via github.
---
+++ weatherapp.js
@@ -0,0 +1,66 @@
+const unirest = require("unirest");
+const express = require("express");
+const bodyText = require("body-parser");
+
+const app = express();
+app.use(bodyText.urlencoded({ extended: true })); //To use body parser with post request
+
+//include all static files so we can use CSS
+app.use(express.static(__dirname + "/public"));
+
+//main page
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Display the information when there is post request
+app.post("/", function (request, response) {
+  //Get the weather data
+  const req = unirest(
+    "GET",
+    "https://community-open-weather-map.p.rapidapi.com/weather"
+  );
+
+  let city = request.body.city;
+  // city = city + ", USA"  //Add country if needed
+
+  req.query({
+    q: city,
+    lang: "en",
+    units: "imperial",
+  });
+
+  // Update your API keys
+  req.headers({
+    "x-rapidapi-key": "e4f0b48b60msh436d402da62c33cp1fa03fjsne9acab908726",
+    "x-rapidapi-host": "openweather43.p.rapidapi.com",
+    useQueryString: true,
+  });
+
+  req.end(function (res) {
+    if (res.error) {
+      if (res.error) throw new Error(res.error);
+    } else {
+      const weather = res.body;
+
+      response.send(`
+  <div class="weather-container">
+    <h2 class="city-heading">Current Weather in ${city}</h2>
+    <div class="weather-details">
+      <p class="temperature">Temperature: ${weather.main.temp} °F</p>
+      <p class="description">Description: ${weather.main[0].description}</p>
+      <p class="humidity">Humidity: ${weather.main.humidity}%</p>
+      
+    </div>
+  </div>
+`);
+
+      response.send(Response);
+    }
+  });
+});
+
+let port = process.env.PORT || 8002;
+app.listen(port, function () {
+  console.log("Server running on port 8002");
+});
